6f072223f1cbbf808cd116280b3203b2b83f00c6
Ray <xiaosumay@gmail.com>
2019-01-15 23:54:58 +0800

    init

---
+++ go-github-server.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"gopkg.in/ini.v1"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+type Repo struct {
+	Path   string `ini:"path"`
+	Key    string `ini:"key,omitempty"`
+	Script string `ini:"cmd,omitempty"`
+	Branch string `ini:"branch,omitempty"`
+}
+
+var (
+	configPath   = flag.String("config", "", "Name of repo to create in authenticated user's GitHub account.")
+	SecretToken  = []byte("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+	repositories = make(map[string]Repo)
+)
+
+func main() {
+	parseConfig()
+
+	http.HandleFunc("/", HandleFunc)
+	err := http.ListenAndServe("0.0.0.0:17293", nil)
+	log.Fatal(err)
+}
+
+func parseConfig() {
+	flag.Parse()
+
+	if _, err := os.Stat(*configPath); err != nil {
+		log.Fatal("请提供配置文件")
+	}
+
+	cfg, err := ini.Load(*configPath)
+	if err != nil {
+		log.Fatal("配置文件出错2")
+	}
+
+	for _, section := range cfg.Sections() {
+		val := new(Repo)
+
+		err = section.MapTo(val)
+		if err != nil {
+			log.Fatalf("配置文件出错3: %v", err)
+		}
+
+		repositories[section.Name()] = *val
+	}
+	//当section空的时候的，一级配置不需要
+	delete(repositories, "DEFAULT")
+}
+
+func HandleFunc(writer http.ResponseWriter, request *http.Request) {
+	data, err := ioutil.ReadAll(request.Body)
+
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	signature := request.Header.Get("X-Hub-Signature")
+
+	if len(signature) == 0 {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte("无效签名"))
+		return
+	}
+
+	mac := hmac.New(sha1.New, SecretToken)
+	_, _ = mac.Write(data)
+	expectedMAC := hex.EncodeToString(mac.Sum(nil))
+
+	if !hmac.Equal([]byte(signature[5:]), []byte(expectedMAC)) {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte("签名不一致"))
+		return
+	}
+
+	event := request.Header.Get("X-GitHub-Event")
+
+	switch strings.ToLower(event) {
+	case "ping":
+		var ping PingPayload
+		err = json.Unmarshal(data, &ping)
+		if err == nil {
+			writer.WriteHeader(http.StatusOK)
+			writer.Write([]byte("连接成功"))
+			return
+		}
+	case "push":
+		PushEvent(data, writer)
+	default:
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte("无效操作"))
+	}
+}
+
+func PushEvent(data []byte, writer http.ResponseWriter) {
+	var push PushPayload
+	err := json.Unmarshal(data, &push)
+	if err != nil {
+		log.Println(err)
+		writer.Write([]byte("不可识别操作"))
+		return
+	}
+
+	repoName := push.Repository.Name
+
+	if repo, ok := repositories[repoName]; ok {
+
+		ref := "refs/heads/" + DefaultValue(repo.Branch, "master")
+		if push.Ref != ref {
+			writer.Write([]byte("非指定分支，不更新"))
+			return
+		} else {
+			go runCommand(repoName, repo)
+
+			writer.Write([]byte("更新成功"))
+		}
+	} else {
+		log.Println(repoName + " 不存在！")
+		return
+	}
+}
+
+func runCommand(repoName string, rep Repo) {
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+
+	script := DefaultValue(rep.Script, dir+"/github_update.sh")
+
+	if _, err := os.Stat(script); err != nil {
+		log.Println(err)
+		return
+	}
+
+	cmd := exec.Command("zsh", script)
+	cmd.Env = append(cmd.Env, "WORK_PATH="+rep.Path, "REPOS="+repoName, "BRANCH="+rep.Branch)
+	if 0 != len(rep.Key) {
+		cmd.Env = append(cmd.Env, fmt.Sprintf(`GIT_SSH_COMMAND="ssh -i %s"`, rep.Key))
+	}
+
+	data, err := cmd.CombinedOutput()
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(string(data))
+}
---
+++ repositories-mgr.go
@@ -0,0 +1,344 @@
+// Copyright 2018 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The newrepo command utilizes go-github as a cli tool for
+// creating new repositoriesDo. It takes an auth token as
+// an enviroment variable and creates the new repo under
+// the account affiliated with that token.
+package main
+
+import (
+	"context"
+	"github.com/google/go-github/github"
+	"github.com/jessevdk/go-flags"
+	"golang.org/x/oauth2"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+)
+
+const (
+	TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+	Owner = "YYYYY"
+)
+
+type CommandOptions struct {
+	Create bool `long:"add" description:"新建"`
+	List   bool `long:"list" description:"查看"`
+	Delete bool `long:"delete" description:"删除"`
+}
+
+type ObjectOptions struct {
+	Repositories  bool `long:"repos" description:"仓库"`
+	Collaborators bool `long:"users" description:"合作者"`
+	DeployKey     bool `long:"deploy" description:"服务器部署key"`
+	WebHook       bool `long:"hook" description:"更新钩子"`
+}
+
+type CommonOptions struct {
+	Name  string `long:"name" description:"仓库名称"`
+	Token string `long:"token" description:"账户Token"`
+}
+
+type RepoOptions struct {
+	Description string `long:"desc" description:"描述"`
+	Private     bool   `long:"private" description:"是否私有仓库"`
+	Since       int64  `long:"since" description:"虽然有这个字段，但我也不知道干啥用的"`
+}
+
+type InviteOptions struct {
+	GithubId string `long:"user-id" description:"被邀请人github的用户ID"`
+}
+
+type DevKeyOptions struct {
+	Id       int64  `long:"deploy-id" description:"远程Key的唯一ID"`
+	Key      string `long:"key" description:"ssh的public key 字符串或文件名"`
+	Title    string `long:"title" description:"key的标题"`
+	ReadOnly bool   `long:"read-only" description:"此key是否只读"`
+}
+
+type WebHookOptions struct {
+	Ip string `long:"ip" description:"更新钩子触发的服务器IP"`
+	Id int64  `long:"hook-id" description:"钩子的唯一ID"`
+}
+
+type Options struct {
+	CommandOpts CommandOptions `group:"Command Options"`
+	ObjectOpts  ObjectOptions  `group:"Object Options"`
+	CommonOpts  CommonOptions  `group:"Common Options"`
+	RepoOpts    RepoOptions    `group:"Repositories Options"`
+	InviteOpts  InviteOptions  `group:"Invite Options"`
+	DevKeyOpts  DevKeyOptions  `group:"DeployKey Options"`
+	WebHookOpts WebHookOptions `group:"WebHook Options"`
+}
+
+func main() {
+
+	var opts Options
+	var parser = flags.NewParser(&opts, flags.Default)
+	_, err := parser.Parse()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	opts.CommonOpts.Token = DefaultValue(
+		DefaultValue(
+			opts.CommonOpts.Token,
+			os.Getenv("GITHUB_AUTH_TOKEN"),
+		),
+		TOKEN,
+	)
+
+	if opts.CommonOpts.Token == "" {
+		log.Fatal("Unauthorized: No token present")
+	}
+
+	if opts.ObjectOpts.Repositories {
+		repositoriesDo(opts)
+	} else if opts.ObjectOpts.Collaborators {
+		collaborators(opts)
+	} else if opts.ObjectOpts.DeployKey {
+		deployKey(opts)
+	} else if opts.ObjectOpts.WebHook {
+		webHook(opts)
+	} else {
+		log.Fatalln("你需要指定一个 Object Options")
+	}
+}
+
+func getClient(token string) (*github.Client, context.Context) {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	return client, ctx
+}
+
+func repositoriesDo(opts Options) {
+
+	client, ctx := getClient(opts.CommonOpts.Token)
+
+	if opts.CommandOpts.Create {
+		options := opts.RepoOpts
+
+		if opts.CommonOpts.Name == "" {
+			log.Fatal("No name: New repos must be given a name")
+		}
+
+		if !options.Private {
+			log.Println("必须开启此项才能是创建私有仓库")
+		}
+
+		r := &github.Repository{
+			Name:        &opts.CommonOpts.Name,
+			Private:     &options.Private,
+			Description: &options.Description,
+		}
+		repo, _, err := client.Repositories.Create(ctx, "", r)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("Successfully created new repo: %v\n", repo.GetSSHURL())
+	} else if opts.CommandOpts.List {
+		reps, resp, err := client.Repositories.List(ctx, "", &github.RepositoryListOptions{
+			Visibility: "all",
+		})
+
+		if err != nil || resp.StatusCode != 200 {
+			log.Fatal(err)
+		}
+
+		for _, rep := range reps {
+			log.Printf("%10s: %s\n", rep.GetName(), rep.GetCloneURL())
+		}
+	} else if opts.CommandOpts.Delete {
+		_, err := client.Repositories.Delete(ctx, Owner, opts.CommonOpts.Name)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("删除" + opts.CommonOpts.Name + "成功!")
+	} else {
+		log.Fatalln("你需要指定一个 Command Options")
+	}
+}
+
+func collaborators(opts Options) {
+	client, ctx := getClient(opts.CommonOpts.Token)
+
+	if opts.CommandOpts.Create {
+		invite := opts.InviteOpts
+
+		if opts.CommonOpts.Name == "" {
+			log.Fatal("No name: New repos must be given a name")
+		}
+
+		_, err := client.Repositories.AddCollaborator(ctx, Owner,
+			opts.CommonOpts.Name,
+			invite.GithubId,
+			&github.RepositoryAddCollaboratorOptions{
+				Permission: "push",
+			})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("https://github.com/MLTechMy/" + opts.CommonOpts.Name + "/invitations")
+	} else if opts.CommandOpts.List {
+		if opts.CommonOpts.Name == "" {
+			log.Fatal("No name: New repos must be given a name")
+		}
+
+		users, _, err := client.Repositories.ListCollaborators(ctx, Owner,
+			opts.CommonOpts.Name,
+			&github.ListCollaboratorsOptions{
+				Affiliation: "all",
+			})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, user := range users {
+			log.Printf("%10s: %s\n", user.GetLogin(), user.GetURL())
+		}
+	} else if opts.CommandOpts.Delete {
+		if opts.CommonOpts.Name == "" {
+			log.Fatal("No name: New repos must be given a name")
+		}
+
+		_, err := client.Repositories.RemoveCollaborator(ctx, Owner,
+			opts.CommonOpts.Name,
+			opts.InviteOpts.GithubId)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("删除合作者:" + opts.InviteOpts.GithubId)
+	} else {
+		log.Fatalln("你需要指定一个 Command Options")
+	}
+}
+
+func deployKey(opts Options) {
+	client, ctx := getClient(opts.CommonOpts.Token)
+
+	if opts.CommandOpts.Create {
+		options := opts.DevKeyOpts
+
+		if !options.ReadOnly {
+			log.Println("必须开启此项才是最安全的")
+		}
+
+		if opts.CommonOpts.Name == "" {
+			log.Fatal("No name: New repos must be given a name")
+		}
+
+		if _, ok := os.Stat(options.Key); ok == nil {
+			tmpKey, err := ioutil.ReadFile(options.Key)
+			if err == nil {
+				options.Key = string(tmpKey)
+			}
+		}
+
+		key, _, err := client.Repositories.CreateKey(ctx, Owner,
+			opts.CommonOpts.Name,
+			&github.Key{
+				Key:      &options.Key,
+				Title:    &options.Title,
+				ReadOnly: &options.ReadOnly,
+			})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("key[%d] %s install ok\n", + key.GetID(), key.GetTitle())
+	} else if opts.CommandOpts.List {
+
+		if opts.CommonOpts.Name == "" {
+			log.Fatal("No name: New repos must be given a name")
+		}
+
+		keys, _, err := client.Repositories.ListKeys(ctx, Owner,
+			opts.CommonOpts.Name,
+			&github.ListOptions{},
+		)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, key := range keys {
+			log.Printf("[%d]: %s\n", key.GetID(), key.GetTitle())
+		}
+
+	} else if opts.CommandOpts.Delete {
+		if opts.CommonOpts.Name == "" {
+			log.Fatal("No name: New repos must be given a name")
+		}
+
+		_, err := client.Repositories.DeleteKey(ctx, Owner, opts.CommonOpts.Name, opts.DevKeyOpts.Id)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("删除DevKey成功！")
+	}
+
+}
+
+func webHook(opts Options) {
+	client, ctx := getClient(opts.CommonOpts.Token)
+
+	if opts.CommonOpts.Name == "" {
+		log.Fatal("No name: New repos must be given a name")
+	}
+
+	if opts.CommandOpts.Create {
+		active := true
+		hookInfo := github.Hook{
+			Config: map[string]interface{}{
+				"url":          "http://" + opts.WebHookOpts.Ip + "/github/postreceive",
+				"content_type": "json",
+				"secret":       "X8dWqLS^6VT&Xy8wZlczvQIuWVyO^<",
+				"insecure_ssl": "0",
+			},
+			Events: []string{"push"},
+			Active: &active,
+		}
+		hook, _, err := client.Repositories.CreateHook(ctx, Owner, opts.CommonOpts.Name, &hookInfo)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("hook 安装: " + strconv.FormatBool(*hook.Active))
+	} else if opts.CommandOpts.List {
+		hooks, _, err := client.Repositories.ListHooks(ctx, Owner, opts.CommonOpts.Name, &github.ListOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, hook := range hooks {
+			log.Printf("%10d, %s\n", hook.GetID(), hook.Config["url"])
+		}
+	} else if opts.CommandOpts.Delete {
+		_, err := client.Repositories.DeleteHook(ctx, Owner, opts.CommonOpts.Name, opts.WebHookOpts.Id)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("删除hook成功!")
+	}
+}
