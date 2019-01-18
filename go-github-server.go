da4689af9328c3999342a04fd139d80b72d2b41c
Ray <xiaosumay@gmail.com>
2019-01-18 13:47:45 +0800

    Update go-github-server.go

--- go-github-server.go
+++ go-github-server.go
@@ -28,7 +28,25 @@ var (
 	configPath   = flag.String("config", "", "Name of repo to create in authenticated user's GitHub account.")
 	SecretToken  = []byte("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 	repositories = make(map[string]Repo)
+	pattern      *regexp.Regexp
 )
+
+func init() {
+	pattern = regexp.MustCompile(`[^\w@%+=:,./-]`)
+}
+
+// Quote returns a shell-escaped version of the string s. The returned value
+// is a string that can safely be used as one token in a shell command line.
+func Quote(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	if pattern.MatchString(s) {
+		return "'" + strings.Replace(s, "'", "'\"'\"'", -1) + "'"
+	}
+
+	return s
+}
 
 func main() {
 	parseConfig()
